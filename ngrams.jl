6616cf53b7c016118ce37f18f8e957a15a00f0b2
Viniciuscoding <vin.idem@gmail.com>
2020-09-27 23:22:26 -0400

    Update ngrams.jl

--- ngrams.jl
+++ ngrams.jl
@@ -5,15 +5,16 @@ module ngrams
     function ngram(text::String, n::Int)
         tokens = split(text)
         bucket = Dict()
+        len = length(tokens) 
         if n < 1
             println("Any number equal or less than 0 is not an ngram.\nPlease add a positive integer number greater than 0 as a valid ngram number.")
-        elseif n > len(tokens)
+        elseif n > len
             println("The number of ngrams is larger than the text body length.\nPlease add a ngram number smaller than your text body length.")
         else
-            if length(tokens) < 1
+            if len < 1
                 println("empty string is not valid")
             else
-                for i in 0:(length(tokens)-n+1)
+                for i in 0:(len-n+1)
                     gram = join(tokens[i:i+n], " ")
                     if gram in bucket
                         bucket[gram] += 1
